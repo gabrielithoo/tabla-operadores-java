ac4563863592646e7733132f82e79eddd3e04c4f
palominodev <gabrielpalominotorres1@gmail.com>
2025-12-14 11:41:00 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/tabla_de_operadores/Tabla_de_operadores.java
@@ -0,0 +1,55 @@
+
+package com.mycompany.tabla_de_operadores;
+import java.util.Scanner;  //esta libreria nos permite escribir en la consola
+
+public class Tabla_de_operadores {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.println("ingresa operador (+ - * /)");
+        String operador = sc.nextLine();
+        System.out.print("ingresa numero: ");
+        int numero = sc.nextInt();
+        System.out.print("ingresa numero desde: ");
+        int desde = sc.nextInt();
+        System.out.print("ingresa numero hasta: ");
+        int hasta = sc.nextInt();
+        System.out.println("tabla de " + operador);
+        switch (operador){
+            case "+":
+                for (int i = desde; i <= hasta; i++){
+                    System.out.println(i + " + " +  numero  + " = " + (numero  + i));
+                }break;
+            case "-":
+                for (int i = desde; i <= hasta; i++){
+                    System.out.println(i + " - " +  numero  + " = " + (numero - i));
+                }break;
+            case "*":
+                for (int i = desde; i <= hasta; i++){
+                    System.out.println(i + " * " +  numero  + " = " + (numero * i));
+                }break;
+         case "/":
+    if (numero == 0) {
+        System.out.println("No se puede dividir entre 0");
+        break;
+    }
+
+    for (int i = desde; i <= hasta; i++) {
+        System.out.println(
+            i + " / " + numero + " = " + ((double) i / numero)
+        );
+    }
+    break;
+
+
+                 
+                    
+                    
+                } 
+       }
+        
+                
+        
+        
+    }
+
